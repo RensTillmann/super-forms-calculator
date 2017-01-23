35fce44c087647c551c3ef8303d11b553eabf267
feeling4design <info@feeling4design.nl>
2017-01-23 23:16:04 +0100

    - Fix: update the data fields attribute after duplicating a column - Fix: update the data fields attribute to make sure regex tags are replaced with according field names

--- super-forms-calculator.php
+++ super-forms-calculator.php
@@ -11,7 +11,7 @@
  * Plugin Name: Super Forms - Calculator
  * Plugin URI:  http://codecanyon.net/item/super-forms-drag-drop-form-builder/13979866
  * Description: Adds an extra element that allows you to do calculations on any of your fields
- * Version:     1.4.3
+ * Version:     1.4.5
  * Author:      feeling4design
  * Author URI:  http://codecanyon.net/user/feeling4design
 */
@@ -37,7 +37,7 @@ if(!class_exists('SUPER_Calculator')) :
          *
          *	@since		1.0.0
         */
-        public $version = '1.4.3';
+        public $version = '1.4.5';
 
 
         /**
@@ -307,6 +307,11 @@ if(!class_exists('SUPER_Calculator')) :
             // @since 1.1.4
             $functions['after_init_calculator_hook'][] = array(
                 'name' => 'conditional_logic'
+            );
+
+            // @since 1.5.0
+            $functions['after_duplicating_column_hook'][] = array(
+                'name' => 'init_calculator_after_duplicating_column'
             );
 
             return $functions;
