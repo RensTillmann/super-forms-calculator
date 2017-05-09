f4ccb20e68c60295d9a13e3254b441bbce5f6da0
feeling4design <info@feeling4design.nl>
2017-05-09 19:20:21 +0200

    'SCRIPT_DEBUG'

--- super-forms-calculator.php
+++ super-forms-calculator.php
@@ -11,7 +11,7 @@
  * Plugin Name: Super Forms - Calculator
  * Plugin URI:  http://codecanyon.net/item/super-forms-drag-drop-form-builder/13979866
  * Description: Adds an extra element that allows you to do calculations on any of your fields
- * Version:     1.6.1
+ * Version:     1.6.2
  * Author:      feeling4design
  * Author URI:  http://codecanyon.net/user/feeling4design
 */
@@ -37,7 +37,7 @@ if(!class_exists('SUPER_Calculator')) :
          *
          *	@since		1.0.0
         */
-        public $version = '1.6.1';
+        public $version = '1.6.2';
 
 
         /**
@@ -382,7 +382,7 @@ if(!class_exists('SUPER_Calculator')) :
          *  @since      1.0.0
         */
         public static function add_stylesheet( $array ) {
-            $suffix         = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+            $suffix         = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.min' : '.min';
             $assets_path    = str_replace( array( 'http:', 'https:' ), '', plugin_dir_url( __FILE__ ) ) . '/assets/';
             $frontend_path   = $assets_path . 'css/frontend/';
             $array['super-calculator'] = array(
@@ -406,7 +406,7 @@ if(!class_exists('SUPER_Calculator')) :
         */
         public static function add_scripts( $array ) {
 
-			$suffix         = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+			$suffix         = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.min' : '.min';
             $assets_path    = str_replace( array( 'http:', 'https:' ), '', plugin_dir_url( __FILE__ ) ) . '/assets/';
             $frontend_path  = $assets_path . 'js/frontend/';
             $array['super-calculator'] = array(
